1154f44e64e377d9597b9f6f26c4ea1888b9e95d
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2026-03-25 11:16:50 +0900

    ignore dist files

--- typescript/dist/index.d.mts
+++
@@ -1,5 +0,0 @@
-//#region src/index.d.ts
-declare function hello(): string;
-//#endregion
-export { hello };
-//# sourceMappingURL=index.d.mts.map
--- typescript/dist/index.mjs
+++
@@ -1,8 +0,0 @@
-//#region src/index.ts
-function hello() {
-	return "hello kumofire/jobs!";
-}
-//#endregion
-export { hello };
-
-//# sourceMappingURL=index.mjs.map
